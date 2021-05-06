6d34f342f640d798ef700f702c085e9b9c413ca2
Janet Ziems <jsziems@gmail.com>
2021-05-06 19:50:45 -0400

    updated code

--- main.js
+++ main.js
@@ -1,25 +1,63 @@
-
-// ToDo: 
-//     Use createElement for the title-thru-expl.  See drawing.
-//     Background image/color?
-//     Font
-//     No image alert
-//     Add copyright
-//     Get rid of image square
+// ToDo:  
+//    Should the display function be after getAndDisplayImage?  Should it be a 'const'?'
+//    Remove console.log
 
 const baseUrl = "https://api.nasa.gov/planetary/apod?api_key=";
 const apiKey = "U5YCwar82d9GKNPxCGPTYasZE30KkEyD2glgTP35"
-//const apiKey = "DEMO_KEY"
+
+const wrapper = document.getElementById("results")
 let url
 
-// ToDo:  Should the display function be after getAndDisplayImage?  Should it be a 'const'?
-const display = (imageUrl, imageExplanation, imageTitle, imageDate) => {
+
+const display = (imageUrl, imageExplanation, imageTitle, imageDate, imageCopyright) => {
+    let imageDivElement
+    let titleElement
+    let dateElement
+    let divElement
+    let imgElement
+    let explanationElement
+
     console.log("in display")
     console.log(imageUrl)
     console.log(imageExplanation)
     console.log(imageTitle)
     console.log(imageDate)
 
+    console.log(wrapper.firstChild)
+    wrapper.removeChild(wrapper.firstChild);
+
+    imageDivElement = document.createElement("div")
+    titleElement = document.createElement("h4")
+    dateElement = document.createElement("h4")
+    divElement = document.createElement("div")
+    imgElement = document.createElement("img")
+    explanationElement = document.createElement("p")
+
+    imageDivElement.className = "image-div"
+    titleElement.className = "img-title"
+    dateElement.className = "img-date"
+    divElement.className = "img-and-expl"
+    imgElement.className = "nasa-img"
+    explanationElement.className = "explanation"
+
+    titleElement.innerText = imageTitle
+    dateElement.innerText = imageDate
+    if (imageCopyright != null){
+        explanationElement.innerHTML = `${imageExplanation} <br> <br>Copyright -- ${imageCopyright}`
+    } else {
+        explanationElement.innerHTML = imageExplanation
+    }
+    imgElement.src = imageUrl
+
+
+    imageDivElement.appendChild(titleElement)
+    imageDivElement.appendChild(dateElement)
+    divElement.appendChild(imgElement)
+    divElement.appendChild(explanationElement)
+    imageDivElement.appendChild(divElement)
+    wrapper.appendChild(imageDivElement)
+
+    /*
     document.getElementById('img-title').innerText = imageTitle
     document.getElementById('img-date').innerText = imageDate
 
@@ -27,6 +65,7 @@ const display = (imageUrl, imageExplanation, imageTitle, imageDate) => {
     image.src = imageUrl;
 
     document.getElementById('explanation').innerHTML = imageExplanation;
+    */
 }
 
 function getAndDisplayImage() {
@@ -47,10 +86,8 @@ function getAndDisplayImage() {
             console.log(json.copyright)
             console.log(json.url)
             if (json.media_type == "image") {
-                display(json.url, json.explanation, json.title, json.date 
-                    )
+                display(json.url, json.explanation, json.title, json.date, json.copyright)
             } else {
-                // ToDo: Clear previous image?
                 alert("Photograph not available for that date.  Please try again.")
             }
         })
